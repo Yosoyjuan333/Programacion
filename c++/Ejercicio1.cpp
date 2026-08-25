7e7bc1a6c796e0805fc00e5298e01c6163f854c0
Yosoyjuan333 <h8117277@gmail.com>
2026-08-24 20:57:34 -0600

    Update Ejercicio1.cpp
    Código1

--- c++/Ejercicio1.cpp
+++ c++/Ejercicio1.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 int main()
 {
-float salario, s_retenido, s_recibido;
-    cout<<"ingresa tu salario mensual";
-    cin>> salario;
+float salario, s_retenido;
+float s_recibido=0;
+cin>>salario;
+    cout<<"ingresa tu salario mensual: ";
 
-    if (salario<=8000)
+    if (salario>=8000)
     {
         s_retenido=salario*0.03;
         s_recibido=salario-s_retenido;
     }
 
-    else if(salario>10000 && salario<=18000)
+    else if(salario>=80000 && salario<=10000)
     {
         s_retenido=salario*0.08;
         s_recibido=salario-s_retenido;
@@ -24,10 +25,16 @@ float salario, s_retenido, s_recibido;
         s_recibido=salario-s_retenido;
     }
 
-    else if (salario>=20000)
+    else if (salario>20000)
     {
         s_retenido=salario*0.15;
         s_recibido=salario-s_retenido;
     }
+
+     cout << "\n--- RESULTADOS ---" << endl;
+    cout << "Salario original: $" << salario << endl;
+    cout << "Descuento aplicado: $" << s_recibido << endl;
+    cout << "Salario final a recibir: $" << (salario - s_recibido) << endl;
+
     return 0;
 }
